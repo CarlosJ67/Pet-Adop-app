f242fe3045d76f4f41219dc3092a59a85a59a2b9
Carlosm <carlosjesus6384@gmail.com>
2024-10-20 21:02:12 -0600

    Filtro de categorias mostrando su informacion en pequeñas cards

--- components/Home/PetListByCategory.jsx
+++ components/Home/PetListByCategory.jsx
@@ -1,36 +1,51 @@
-import { View, Text } from 'react-native'
-import React, { useState } from 'react'
+import { View, Text, FlatList } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import Category from './Category'
 import { collection, doc, getDocs, query, where } from 'firebase/firestore'
 import { db } from '../../config/FirebaseConfig'
+import PetListItem from './PetListItem'
 
 export default function PetListByCategory() {
 
   const [petList,setPetList]=useState([]);
+  const [loader, setLoader]=useState(false);
+
+  useEffect(()=>{
+    GetPetList('Perros')
+  }, [])
 
   /**
    *  Se utiliza para obtener la lista de la categoria seleccionada. 
    * @param {*} category 
    */
   const GetPetList=async(category)=>{
-    const q=query(collection(db,'Pets'), where('category','==',category?category:'Perros'));
+    setLoader(true)
+    setPetList([]);
+    const q=query(collection(db,'Pets'), where('category','==',category));
     const querySnapshot=await getDocs(q);
 
     querySnapshot.forEach(doc=>{
       console.log(doc.data());
       setPetList(petList=>[...petList,doc.data()])
     })
-  }
 
+    setLoader(false);
+  }
 
   return (
     <View>
       <Category category={(value)=>GetPetList(value)}/>
-      
 
-
-
-      
+      <FlatList
+        data={petList}
+        style={{marginTop:10}}
+        horizontal={true}
+        refreshing={loader}
+        onRefresh={()=>GetPetList('Perros')}
+        renderItem={({item,index})=>(
+          <PetListItem pet={item} />
+        )}
+      />
     </View>
   )
 }
